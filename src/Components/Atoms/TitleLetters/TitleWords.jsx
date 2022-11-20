ec35259e5859bedbf3ab8bf35dc0c3327d0d8305
NachoBerridy <nachoberridy1997@gmail.com>
2022-11-20 19:33:18 -0300

    Title style

--- src/Components/Atoms/TitleLetters/TitleWords.jsx
+++ src/Components/Atoms/TitleLetters/TitleWords.jsx
@@ -3,13 +3,29 @@ import React from "react";
 
 const TitleWords = ({ title }) => {
 
-    
+    const words = title.split(" ");
 
     return (
         <div className="flex font-black ">
-            {title.split("").map((letter, index) => (
-                <span className=" hover:text-lime-600"  key={index}>{letter}</span>
-            ))}
+            {
+                words.map((word, index) => {
+                    return (
+                        <div className="flex ml-2" key={index}>
+                            {
+                                word.split("").map((letter, index) => {
+                                    return (
+                                        <div className="flex flex-col" key={index}>
+                                            <div className="flex flex-col">
+                                                <span className="text-5xl text-white hover:cursor-default hover:scale-110">{letter}</span>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 }
